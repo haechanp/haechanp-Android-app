6679efa5031c0f9d4578e24b32e15b84221d7607
haechanp <144800399+haechanp@users.noreply.github.com>
2024-04-07 20:03:53 +0900

    Add files via upload

---
+++ weeks2/Dice/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.dice;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void rollDice() {
+        Dice dice = new Dice(6);
+        int diceRoll = dice.roll();
+
+        ImageView diceImage = findViewById(R.id.imageView);
+
+        switch (diceRoll) {
+            case 1:
+                diceImage.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                diceImage.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                diceImage.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                diceImage.setImageResource(R.drawable.dice4);
+                break;
+            case 5:
+                diceImage.setImageResource(R.drawable.dice5);
+                break;
+            case 6:
+                diceImage.setImageResource(R.drawable.dice6);
+                break;
+        }
+    }
+
+    public void onRollButtonClick(View view) {
+        rollDice();
+    }
+
+    public class Dice {
+        private int sides;
+
+        public Dice(int sides) {
+            this.sides = sides;
+        }
+
+        public int roll() {
+            return (int) (Math.random() * sides) + 1;
+        }
+    }
+}
